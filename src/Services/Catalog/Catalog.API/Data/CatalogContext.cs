510acfcd0b6bd248f44a0630724ef5ed814836fd
Jacques Mbuyamba <JMbuyamba@SINGULAR.CO.ZA>
2022-04-06 23:28:01 +0200

    Finish to create the catalog APi project

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Data;
+
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+public class CatalogContext : ICatalogContext
+{
+
+  public CatalogContext(IConfiguration config)
+  {
+    var client = new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"));
+    var database = client.GetDatabase(config.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+    Products = database.GetCollection<Product>(config.GetValue<string>("DatabaseSettings:CollectionName"));
+
+    CatalogContextSeed.SeedData(Products);
+  }
+  public IMongoCollection<Product> Products { get; }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities;
+
+public class Product
+{
+  [BsonId]
+  [BsonRepresentation(BsonType.ObjectId)]
+  public string? Id { get; set; }
+
+  [BsonElement("Name")]
+  public string? Name { get; set; }
+  public string? Category { get; set; }
+  public string? Summary { get; set; }
+
+  public string? Description { get; set; }
+  public string? ImageFile { get; set; }
+  public long Price { get; set; }
+  public int QunatityInStock { get; set; }
+  public string? Brand { get; set; }
+  public string? Type { get; set; }
+  public DateTime CreatedDate { get; set; } = DateTime.Now;
+  public DateTime LastModified { get; set; } = DateTime.Now;
+  public string? ModifiedBy { get; set; }
+  public string? Warranty { get; set; }
+  public bool IsTaxable { get; set; }
+  public ProductOption? Option { get; set; }
+  public ProductStore? Store { get; set; }
+
+}
+
+public class ProductOption
+{
+  public string? Size { get; set; }
+  public string? Feature { get; set; }
+  public string? Colour { get; set; }
+  public string? ImageFile { get; set; }
+  public int Quantity { get; set; }
+}
+
+public class ProductStore
+{
+  public string? Id { get; set; }
+  public string? StoreName { get; set; }
+  public string? Address { get; set; }
+}
+
