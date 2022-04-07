c00fc1b51446db99a9214e3f7ecebec50f650f4b
Jacques Mbuyamba <JMbuyamba@SINGULAR.CO.ZA>
2022-04-07 18:38:02 +0200

    complete with basket controller services

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿namespace Basket.API.Entities
+{
+  public class ShoppingCart
+  {
+    public string? UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+    public ShoppingCart()
+    {
+
+    }
+
+    public ShoppingCart(string username)
+    {
+      UserName = username;
+    }
+
+    public long TotalPrice
+    {
+      get
+      {
+        long totalprice = 0;
+        foreach (var item in Items)
+        {
+          totalprice += item.Price * item.Quantity;
+        }
+        return totalprice;
+      }
+    }
+  }
+}
