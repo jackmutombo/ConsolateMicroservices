08c1a16b86779d30f5858866b9cd2b6764f68fd2
Jacques Mbuyamba <JMbuyamba@SINGULAR.CO.ZA>
2022-04-10 13:46:09 +0200

    finish adding event bus on basket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,15 +11,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Redis Configuration
 builder.Services.AddStackExchangeRedisCache(option =>
 {
   option.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
+// General Configuartion
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+//Grpc Configuartion
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opt =>
                           opt.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 builder.Services.AddScoped<DiscountGrpcService>();
+
+// MassTrabsit-RabbitMq Configuration
+builder.Services.AddMassTransit(config =>
+{
+  config.UsingRabbitMq((ctx, cfg) =>
+  {
+    cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+  });
+});
+
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 
