21b4094ba28ed17851ce32e92cd2fdbc6f5191bb
Jacques Mbuyamba <JMbuyamba@SINGULAR.CO.ZA>
2022-04-11 21:26:01 +0200

    End of OcelotApiGateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,20 +1,23 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Host.ConfigureLogging((hostingContext, loggingbuilder) =>
 {
+  builder.Configuration.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
   loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
   loggingbuilder.AddConsole();
   loggingbuilder.AddDebug();
 });
 
-builder.Services.AddOcelot();
-
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddOcelot()
+  .AddCacheManager(settings => settings.WithDictionaryHandle());
+var app = builder.Build();
 
 await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
